7ff45c1c9ffa6aa06ee28588bea8d3384055c1cb
zhouyong <zhouyong@chinac.com>
2017-02-27 11:53:02 +0800

    init project.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.less';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="neo-footer">
+        footer
+      </div>
+    );
+  }
+}
+
+export default Footer;
